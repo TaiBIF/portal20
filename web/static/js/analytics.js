7d503f80f95b419f45b9582d276eeedcedf2416e
MooGoo <moogoo78@gmail.com>
2020-01-09 13:37:26 +0800

    tune data charts

--- web/static/js/analytics.js
+++ web/static/js/analytics.js
@@ -1,7 +1,7 @@
 
-var width = 960;
+var width = 980;
 var height = 380;
-var padding = 60;
+var padding = 80;
 
 function renderBarChart(selector, dataset) {
 
@@ -18,10 +18,12 @@ function renderBarChart(selector, dataset) {
 
   svg.append("g")
     .attr("transform", "translate(" + 0 + "," + (height - padding) + ")")
+    .attr("class", "axis")
     .call(d3.axisBottom(xScale));
 
   svg.append("g")
     .attr("transform", "translate(" + padding + "," + 0 + ")")
+    .attr("class", "axis")
     .call(d3.axisLeft(yScale).ticks(8));
 
   svg.append("g")
@@ -60,13 +62,13 @@ d3.json(dataURL).then( data => {
 //////////////////////////
 
 function renderLineChart(selector, dataset) {
-  console.log(dataset);
+  //console.log(dataset);
   var svg = d3.select(selector).append("svg").attr("width", width).attr("height", height);
 
   var xScale = d3.scaleBand()
     .rangeRound([padding, width - padding])
     .padding(0.5)
-    .domain(dataset.map(function(d) { return d.year; }));
+      .domain(dataset.map(function(d) {  return d.year; }));
 
   var yScale = d3.scaleLinear()
     .domain([0, d3.max(dataset, function(d) { return d.y2; })])
@@ -74,28 +76,51 @@ function renderLineChart(selector, dataset) {
 
   svg.append("g")
     .attr("transform", "translate(" + 0 + "," + (height - padding) + ")")
+    .attr("class", "axis")
     .call(d3.axisBottom(xScale));
 
   svg.append("g")
     .attr("transform", "translate(" + padding + "," + 0 + ")")
+    .attr("class", "axis")
     .call(d3.axisLeft(yScale).ticks(8));
 
+  let hackYSHIFT = 26;
   svg.append("path")
     .datum(dataset)
     .attr("fill", "none")
     .attr("stroke", "#bb998b")
-    .attr("stroke-width", 1.5)
+    .style("stroke-dasharray", ("3, 3"))
+    .attr("stroke-width", 3)
     .attr("d", d3.line()
-      .x(function(d) { return xScale(d.year); })
+          .x(function(d) { return xScale(d.year)+hackYSHIFT; })
           .y(function(d) { return yScale(d.y1); }));
- 
+
   svg.append("path")
     .datum(dataset)
     .attr("fill", "none")
     .attr("stroke", "#846C5B")
-    .attr("stroke-width", 1.5)
+    .attr("stroke-width", 3)
     .attr("d", d3.line()
-      .x(function(d) { return xScale(d.year); })
+          .x(function(d) { return xScale(d.year)+hackYSHIFT; })
           .y(function(d) { return yScale(d.y2); }));
 
+  svg.append("g")
+    .selectAll("path")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .attr("x", function(d) {return xScale(d.year)+14; })
+    .attr("y", function(d) {return yScale(d.y2)-20 })
+    .text(function(d) {return d.y2.toLocaleString()})
+    .attr("class", "label");
+
+  svg.append("g")
+    .selectAll("path")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .attr("x", function(d) { console.log(d);return xScale(d.year)+24; })
+    .attr("y", function(d) {return yScale(d.y1)-3; })
+    .text(function(d) {return d.y1.toLocaleString()})
+    .attr("class", "label");
 }
