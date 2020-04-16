162b61ff477dcac34661cd12f7b48779c8339de3
MooGoo <moogoo78@gmail.com>
2020-04-17 03:15:04 +0800

    add redis, home_stats

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7.2-slim
+FROM python:3.8.2-slim
 
 # set environment varibles
 ENV PYTHONDONTWRITEBYTECODE 1
--- conf/settings.py
+++ conf/settings.py
@@ -208,7 +208,16 @@ LOGGING = {
 #USE_SENTRY=on
 #SENTRY_DSN=https://<project-key>@sentry.io/<project-id>
 
-
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "taibif"
+    }
+}
 # email
 EMAIL_BACKEND = 'django_ses.SESBackend'
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
