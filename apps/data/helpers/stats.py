67e7e6cd831083981f55d83cac50df2a67f25f50
MooGoo <moogoo78@gmail.com>
2020-04-17 16:31:53 +0800

    forgot add files

---
+++ apps/data/helpers/stats.py
@@ -0,0 +1,23 @@
+from django.db.models import (
+    Count,
+    Sum,
+)
+from apps.data.models import (
+    Dataset,
+    SimpleData,
+)
+from utils.general import get_cache_or_set
+
+def _get_home_stats():
+    dataset_stats = Dataset.public_objects.aggregate(num_occur=Sum('num_occurrence'), num_dataset=Count('name'))
+    species_count = SimpleData.public_objects.values('scientific_name').annotate(total=Count('scientific_name')).count()
+    #a = SimpleData.public_objects.aggregate(total=Count('scientific_name')) # 4456372 ? why?
+    stats = {
+        'num_occurrence': dataset_stats['num_occur'],
+        'num_dataset': dataset_stats['num_dataset'],
+        'num_species': species_count,
+    }
+    return stats
+
+def get_home_stats():
+    return get_cache_or_set('home_stats', _get_home_stats)
