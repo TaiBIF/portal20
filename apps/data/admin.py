e959493f99d09d693c56fe2a0f30934b72ef3f3e
MooGoo <moogoo78@gmail.com>
2020-04-23 17:35:38 +0800

    [SCHEMA] dataset add column

--- apps/data/admin.py
+++ apps/data/admin.py
@@ -6,7 +6,7 @@ class DatasetAdmin(admin.ModelAdmin):
     model = Dataset
     list_display = ('title', 'name', 'num_occurrence', 'pub_date', 'guid')
     list_filter = ('is_most_project', 'dwc_core_type')
-    fields = ('title', 'name', 'description','author', 'pub_date', 'guid', 'dwc_core_type', 'num_record', 'num_occurrence', 'extension_data', 'is_most_project')
+    fields = ('title', 'name', 'description','author', 'pub_date', 'guid', 'dwc_core_type', 'num_record', 'num_occurrence', 'extension_data', 'is_most_project', 'has_publish_problem', 'admin_memo')
     readonly_fields = ('title', 'name', 'description','author', 'pub_date', 'guid', 'dwc_core_type', 'num_record', 'num_occurrence', 'extension_data')
     search_fields = ('title',)
 
