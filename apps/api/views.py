463439f7fcf733afd27167699abe160f3b82d475
Leonardo Jefferson <jeffersonk.tw@gmail.com>
2021-09-24 06:23:54 +0800

    modified : solr search

--- apps/api/views.py
+++ apps/api/views.py
@@ -104,24 +104,38 @@ def occurrence_search_v2(request):
     resp['elapsed'] = time.time() - time_start
     return JsonResponse(resp)
 
+
 def search_occurrence_v1_charts(request):
-    year_start = z1000
+    year_start = 1000
     year_end = 2021
 
+    solr_q_fq_list=[]
+    solr_fq = ''
     solr_q_list = []
     solr_q = '*:*'
     for term, values in list(request.GET.lists()):
-        if term != 'menu':
-            if term =='year':
-                val = values[0].replace(",", " TO ")
-                solr_q_list.append('{}= {}'.format(term,val))
+        if term !='q' :
+            if term != 'menu':
+                if term =='year':
+                    val = values[0].replace(",", " TO ")
+                    solr_q_fq_list.append('{}:[{}]'.format(term,val))
+                    year_start =values[0].split(',',1)
+                    year_end =values[0].split(',',2)
+                elif term =='dataset':
+                    solr_q_fq_list.append('{}:"{}"'.format('taibif_dataset_name_zh', '" OR "'.join(values)))
+                elif term =='month':
+                    solr_q_fq_list.append('{}:{}'.format(term, ' OR '.join(values)))
 
-                year_start =values[0].split(',',1)
-                year_end =values[0].split(',',2)
-            else :
-                solr_q_list.append('{}={}'.format(term, ' OR '.join(values)))
+        else:
+            solr_q_list.append('{}:{}'.format('_text_', ' OR '.join(values)))
+
+
     if len(solr_q_list) > 0:
-        solr_q = ', '.join(solr_q_list)
+        solr_q = ' AND '.join(solr_q_list)
+
+    if len(solr_q_fq_list) > 0:
+        solr_fq = ' AND '.join(solr_q_fq_list)
+    print(solr_fq)
 
     charts_year = []
     charts_month = []
@@ -130,36 +144,34 @@ def search_occurrence_v1_charts(request):
     search_count = 0
     search_offset = 0
     search_results = []
-    #publisher_query = Dataset.objects\
-    #                         .values('organization','organization_verbatim')\
-    #                         .exclude(organization__isnull=True)\
-    #                         .annotate(count=Count('organization'))\
-    #                         .order_by('-count')
-    #menu_publisher = [{
-    #    'key':x['organization'],
-    #    'label':x['organization_verbatim'],
-    #    'count': x['count']
-    #} for x in publisher_query]
-    
     
 
     time_start = time.time()  
-    facet_dataset = 'dataset:{type:terms,field:taibif_dataset_name_zh,limit:-1,mincount:1}'
+    facet_dataset = 'dataset:{type:terms,field:taibif_dataset_name_zh,limit:-1,mincount:0}'
     facet_month = 'month:{type:range,field:month,start:1,end:13,gap:1}'
-    facet_year = 'year:{type:terms,field:year,limit:-1,mincount:1}'
+    facet_year = 'year:{type:terms,field:year,limit:-1,mincount:0}'
     facet_json = 'json.facet={'+facet_dataset + ',' +facet_month+ ',' +facet_year+'}'
-    r = requests.get(f'http://solr:8983/solr/taibif_occurrence/select?facet=true&q.op=OR&q={solr_q}&{facet_json}')
+    r = requests.get(f'http://solr:8983/solr/taibif_occurrence/select?facet=true&q.op=OR&q={solr_q}&fq={solr_fq}&{facet_json}')
 
     if r.status_code == 200:
         data = r.json()
 
-        search_count = data['response']['numFound']
-        search_offset = data['response']['start']
-        search_results = data['response']['docs']
 
-        charts_year =[{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['year']['buckets']]
-        charts_month = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['month']['buckets']]
-        charts_dataset = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['dataset']['buckets']]
+        search_count = data['response']['numFound']
+        
+        if search_count != 0:
+            search_offset = data['response']['start']
+            search_results = data['response']['docs']
+
+            charts_year =[{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['year']['buckets']]
+            charts_month = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['month']['buckets']]
+            charts_dataset = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['dataset']['buckets']]
+        else:
+            charts_year = [{'key': 0, 'label': 0, 'count': 0}]
+            charts_month = [{'key': 0, 'label': 0, 'count': 0}]
+            charts_dataset = [{'key': 0, 'label': 0, 'count': 0}]
+
+
 
     ret = {
         'charts': [
@@ -180,8 +192,10 @@ def search_occurrence_v1_charts(request):
             },
         ],
     }
-
     return JsonResponse(ret)
+
+
+
 
 
 
@@ -250,35 +264,43 @@ def search_occurrence_v1(request):
     if r.status_code == 200:
         data = r.json()
 
-        search_count = data['response']['numFound']
-        search_offset = data['response']['start']
-        search_results = data['response']['docs']
-
-        for i, v in enumerate(search_results):
+        if search_count != 0:
+            search_offset = data['response']['start']
+            search_results = data['response']['docs']
+            for i, v in enumerate(search_results):
             ## copy fields
-            date = '{}-{}-{}'.format(v['year'] if v.get('year', '') else '',
-                                     v['month'] if v.get('month', '') else '',
-                                     v['day'] if v.get('day', '') else '')
-            search_results[i]['vernacular_name'] = v.get('vernacularName', '')
-            search_results[i]['scientific_name'] = v.get('scientificName', '')
-            search_results[i]['dataset'] = v['taibif_dataset_name']
-            search_results[i]['date'] = date
-            search_results[i]['taibif_id'] = '{}__{}'.format(v['taibif_dataset_name'], v['_version_'])
-            search_results[i]['kingdom'] = v.get('kingdom_zh', '')
-            search_results[i]['phylum'] = v.get('phylum_zh', '')
-            search_results[i]['class'] = v.get('class_zh', '')
-            search_results[i]['order'] = v.get('order_zh', '')
-            search_results[i]['family'] = v.get('family_zh', '')
-            search_results[i]['genus'] = v.get('genus_zh', '')
-            search_results[i]['species'] = v.get('species_zh', '')
+                date = '{}-{}-{}'.format(v['year'] if v.get('year', '') else '',
+                                        v['month'] if v.get('month', '') else '',
+                                        v['day'] if v.get('day', '') else '')
+                search_results[i]['vernacular_name'] = v.get('vernacularName', '')
+                search_results[i]['scientific_name'] = v.get('scientificName', '')
+                search_results[i]['dataset'] = v['taibif_dataset_name']
+                search_results[i]['date'] = date
+                search_results[i]['taibif_id'] = '{}__{}'.format(v['taibif_dataset_name'], v['_version_'])
+                search_results[i]['kingdom'] = v.get('kingdom_zh', '')
+                search_results[i]['phylum'] = v.get('phylum_zh', '')
+                search_results[i]['class'] = v.get('class_zh', '')
+                search_results[i]['order'] = v.get('order_zh', '')
+                search_results[i]['family'] = v.get('family_zh', '')
+                search_results[i]['genus'] = v.get('genus_zh', '')
+                search_results[i]['species'] = v.get('species_zh', '')
+
+            menu_year = [{'key': 0, 'label': 0, 'count': 0,'year_start':year_start,'year_end':year_end}]
+            menu_month = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['month']['buckets']]
+            menu_dataset = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['dataset']['buckets']]
+            menu_country = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['country']['buckets']]
+            menu_publisher = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['publisher']['buckets']]
+        else:
+            menu_year = [{'key': 0, 'label': 0, 'count': 0,'year_start':year_start,'year_end':year_end}]
+            menu_month = [{'key': x, 'label': x, 'count': 0} for x in range(12)]
+            menu_dataset = [{'key': 0, 'label': 0, 'count': 0}]
+            menu_country = [{'key': 0, 'label': 0, 'count': 0}]
+            menu_publisher = [{'key': 0, 'label': 0, 'count': 0}]
+
+        
 
         #search_limit = 20
-        menu_year = [{'key': 0, 'label': 0, 'count': 0,'year_start':year_start,'year_end':year_end}]
-        menu_month = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['month']['buckets']]
-        menu_dataset = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['dataset']['buckets']]
-        menu_country = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['country']['buckets']]
-        menu_publisher = [{'key': x['val'], 'label': x['val'], 'count': x['count']} for x in data['facets']['publisher']['buckets']]
-
+        
     ret = {
         'menus': [
             {
