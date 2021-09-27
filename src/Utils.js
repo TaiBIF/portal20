504794ce78f5c1d283ecd07cf44921a749ef1a8b
MooGoo <moogoo78@gmail.com>
2021-09-27 16:05:50 +0800

    [frontend] add pagination

--- src/Utils.js
+++ src/Utils.js
@@ -8,16 +8,80 @@ async function fetchData(url) {
   return data;
 }
 
-function filtersToSearch (filters) {
+const filtersToSearch = (filters, removeOffset=false) => {
   //TODO: Array.from(filters).join('&');
+  //console.log(removeOffset);
   const qsArr = [];
   filters.forEach((item)=> {
-    qsArr.push(item);
+    const key = item.split('=')[0];
+    if (removeOffset === false) {
+      qsArr.push(item);
+    } else if (key != 'offset') {
+      qsArr.push(item);
+    }
   });
   return qsArr.join('&');
 }
 
-const Pagination = ({data, filters, onClick}) => {
+const appendUrl = (url, queryString) => {
+  if (url.indexOf('?') >= 0) {
+    return `${url}&${queryString}`;
+  } else {
+    return `${url}?${queryString}`;
+  }
+}
+const Pagination = ({total, offset=0, urlPrefix}) => {
+  // fixed
+  const limit = 20;
+  const numPageDisplay = 5
+  const offsetLimit = limit * numPageDisplay;
+  // count
+  const currentPage = Math.ceil(offset / limit) + 1;
+  const lastPage = Math.ceil(total / limit);
+  const stepLimit = Math.min(lastPage, numPageDisplay);
+
+  const pageStart = (Math.floor(offset/offsetLimit) * numPageDisplay) + 1;
+  let step = 0;
+  const offsetStart = Math.floor(offset/offsetLimit) * offsetLimit;
+  let pageOffset = offsetStart;
+  const pageList = [];
+  while (step < stepLimit && pageOffset < total) {
+    const url = appendUrl(urlPrefix, `offset=${pageOffset}`);
+    pageList.push(<li key={urlPrefix+step} className={(offset === pageOffset) ? 'active' : null}><a href={url}>{step+pageStart}</a></li>);
+    step++;
+    pageOffset += limit;
+  }
+
+  return (
+      <div className="center-block text-center">
+      <ul className="pagination">
+      <li>
+      <a href={urlPrefix} aria-label="Previous">
+      <span aria-hidden="true">&laquo;</span>
+      </a>
+      </li>
+      { (offsetStart - limit >= 0) ?
+        <li><a href={appendUrl(urlPrefix, `offset=${offsetStart-limit}`)}>...</a></li>
+        : null}
+      {pageList}
+      { (pageOffset + limit < total) ?
+        <li><a href={appendUrl(urlPrefix, `offset=${offsetStart+limit}`)}>...</a></li>
+        : null}
+      <li>
+      { (pageOffset + limit < total ) ?
+        <a href={appendUrl(urlPrefix, `offset=${pageOffset+limit}`)}  aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+        </a>
+        : null
+      }
+      </li>
+      </ul>
+      </div>
+  );
+}
+
+
+const Pagination2 = ({data, filters, onClick}) => {
   //console.log(data, filters)
   const { offset, limit, count } = data;
   const currentPage = Math.ceil(offset / limit) + 1;
