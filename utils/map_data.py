081160f6f53e6a91dacae1bc27548c7f598ce775
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-10-07 14:50:54 +0800

    set default geojson & count to redis cache

--- utils/map_data.py
+++ utils/map_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import bisect
+import requests
 
 list_x = np.arange(-180, 180, 0.01)
 list_y = np.arange(-90, 90, 0.01)
@@ -26,3 +27,34 @@ def convert_x_coor_to_grid(x):
 def convert_y_coor_to_grid(y):
   grid_y = bisect.bisect(list_y, y)-1
   return grid_y
+
+
+def get_geojson(solr_url):
+    facet_pivot_map = 'facet.pivot=grid_x,grid_y'
+    if 'grid_x' in solr_url:
+        map_url = f'{solr_url}&facet=true&{facet_pivot_map}&facet.limit=-1'
+    else:
+        map_url = f'{solr_url}&facet=true&fq=grid_x%3A%5B0%20TO%20*%5D&fq=grid_y%3A%5B0%20TO%20*%5D&{facet_pivot_map}&facet.limit=-1'
+    map_url = map_url.replace('rows=20','rows=0')        
+    r = requests.get(map_url)
+    data_c = {}
+    if r.status_code == 200:
+        data = r.json()
+        data_c = data['facet_counts']['facet_pivot']['grid_x,grid_y']
+
+    map_geojson = {"type":"FeatureCollection","features":[]}
+    for i in data_c:
+        current_grid_x = i['value']
+        for j in i['pivot']:
+            current_grid_y = j['value']
+            current_count = j['count']
+            current_center_x, current_center_y = convert_grid_to_coor(current_grid_x, current_grid_y)
+            tmp = [{
+                "type": "Feature",
+                "geometry":{"type":"Point","coordinates":[current_center_x,current_center_y]},
+                "properties": {
+                    "counts": current_count
+                }
+            }]
+            map_geojson['features'] += tmp
+    return map_geojson
