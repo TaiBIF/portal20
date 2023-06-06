ae188dd65a0729b6821387abfc52b1003b0d1e7d
Leonardo Jefferson <jeffersonk.tw@gmail.com>
2023-06-06 11:32:58 +0800

    remove deprecated url

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,14 +4,13 @@ from . import views
 
 urlpatterns = [
     path('dataset/search/', views.search_dataset, name='api-search-dataset'),
-    #path('occurrence/search/', views.search_occurrence, name='api-search-occurrence'),
     re_path('^occurrence/(?P<cat>search|taxonomy|charts|gallery|map|download)/', views.search_occurrence_v1, name='api-search-occurrence'),
     path('publisher/search/', views.search_publisher, name='api-search-publisher'),
     path('species/search/', views.search_species, name='api-search-species'),
-    path('species/<int:pk>/', views.species_detail, name='species-detail'),
+    # path('species/<int:pk>/', views.species_detail, name='species-detail'),
     path('taxon/tree/node/<str:taicol_taxon_id>', views.taxon_tree_node, name='taxon-tree-branch'),
     path('data/stats/', views.data_stats, name='data-stats'),
-    path('taxon_bar/', views.taxon_bar, name='bar_chart'), ## TODO
+    # path('taxon_bar/', views.taxon_bar, name='bar_chart'), ## TODO
     path('v1/occurrence', views.search_occurrence_v1, name='search-occurrence-v1'),
     path('v1/occurrence/charts', views.occurrence_search_v2, name='search-occurrence-v1-charts'),
     path('v2/occurrence/search', views.occurrence_search_v2, name='api-occurrence-search-v2'),
