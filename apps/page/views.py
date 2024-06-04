b75aeec62a49164b84d8990fd27b12fa5a2e4862
JJJHANG <jangzhu5678@gmail.com>
2024-06-04 09:53:02 +0800

    update: sort table contents by pub date in data_stat page

--- apps/page/views.py
+++ apps/page/views.py
@@ -230,10 +230,9 @@ def data_stats(request):
     dataset_num = Dataset.objects.filter(status='PUBLIC').count()
     publisher_num = DatasetOrganization.objects.count()
 
-    dataset_orm = Dataset.objects.filter(source='TaiBIF IPT', status='PUBLIC')
+    dataset_orm = Dataset.objects.filter(source='TaiBIF IPT', status='PUBLIC').order_by('pub_date')
     # Grab the content for the table
     if search_query:
-        print('yes')
         dataset = dataset_orm.filter(Q(title__contains=search_query) | Q(dwc_core_type__contains=search_query)).values('title', 'organization_name', 'dwc_core_type', 'num_occurrence', 'num_record', 'pub_date', 'country', 'status', 'is_most_project', 'taibif_dataset_id')
     else:
         dataset = dataset_orm.values('title', 'organization_name', 'dwc_core_type', 'num_occurrence', 'num_record', 'pub_date', 'country', 'status', 'is_most_project', 'taibif_dataset_id')
