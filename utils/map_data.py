71bdbb8b8f7dfb2b5020da61957b90f87e345a58
JJJHANG <jangzhu5678@gmail.com>
2024-06-07 13:44:01 +0800

    update: refactor map_data

--- utils/map_data.py
+++ utils/map_data.py
@@ -11,8 +11,8 @@ def convert_grid_to_coor(grid_x, grid_y):
         grid_x = 35998
     if grid_y >= 17999:
         grid_y = 17998
-    center_x = (list_x[grid_x] + list_x[grid_x+1])/2
-    center_y = (list_y[grid_y] + list_y[grid_y+1])/2
+    center_x = round((list_x[grid_x] + list_x[grid_x+1])/2, 2)
+    center_y = round((list_y[grid_y] + list_y[grid_y+1])/2, 2)
     return center_x, center_y
 
 def convert_coor_to_grid(x, y):
@@ -32,29 +32,20 @@ def convert_y_coor_to_grid(y):
 def get_geojson(solr_url):
     facet_pivot_map = 'facet.pivot=grid_x,grid_y'
     if 'grid_x' in solr_url:
-        map_url = f'{solr_url}&facet=true&{facet_pivot_map}&facet.limit=-1'
+        map_url = f'{solr_url}&rows=0&facet=true&{facet_pivot_map}&facet.limit=-1'
     else:
-        map_url = f'{solr_url}&facet=true&fq=grid_x%3A%5B0%20TO%20*%5D&fq=grid_y%3A%5B0%20TO%20*%5D&{facet_pivot_map}&facet.limit=-1'
-    map_url = map_url.replace('rows=20','rows=0')        
+        map_url = f'{solr_url}&rows=0&facet=true&fq=grid_x%3A%5B0%20TO%20*%5D&fq=grid_y%3A%5B0%20TO%20*%5D&{facet_pivot_map}&facet.limit=-1'    
     r = requests.get(map_url)
-    data_c = {}
-    if r.status_code == 200:
-        data = r.json()
-        data_c = data['facet_counts']['facet_pivot']['grid_x,grid_y']
+    map_geojson = {"type": "FeatureCollection", "features": []}
 
-    map_geojson = {"type":"FeatureCollection","features":[]}
-    for i in data_c:
-        current_grid_x = i['value']
-        for j in i['pivot']:
-            current_grid_y = j['value']
-            current_count = j['count']
-            current_center_x, current_center_y = convert_grid_to_coor(current_grid_x, current_grid_y)
-            tmp = [{
+    if r.status_code == 200:
+        data_c = r.json()['facet_counts']['facet_pivot']['grid_x,grid_y']
+        map_geojson['features'] = [
+            {
                 "type": "Feature",
-                "geometry":{"type":"Point","coordinates":[current_center_x,current_center_y]},
-                "properties": {
-                    "counts": current_count
-                }
-            }]
-            map_geojson['features'] += tmp
+                "geometry": {"type": "Point", "coordinates": convert_grid_to_coor(i['value'], j['value'])},
+                "properties": {"counts": j['count']}
+            }
+            for i in data_c for j in i['pivot']
+        ]
     return map_geojson
