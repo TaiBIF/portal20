47a4684868923cf540911cfa70650d7d13ac3a34
JJJHANG <jangzhu5678@gmail.com>
2026-03-16 13:31:04 +0800

    index bubble in home page

--- apps/page/admin.py
+++ apps/page/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Post, Journal 
+from .models import Post, Journal, IndexBubbleSetting
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Journal)
+
+
+class IndexBubbleSettingAdmin(admin.ModelAdmin):
+    list_display = ("is_active", "message", "url", "updated_at")
+    fields = ("is_active", "message", "url")
+
+    def has_add_permission(self, request):
+        return not IndexBubbleSetting.objects.exists()
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(IndexBubbleSetting, IndexBubbleSettingAdmin)
--- apps/page/models.py
+++ apps/page/models.py
@@ -72,6 +72,32 @@ class Journal(models.Model):
         ordering = ['sort',]
 
 
+class IndexBubbleSetting(models.Model):
+    message = models.CharField(u"說話魚文案", max_length=300, default="活動資訊活動資訊活動資訊活動資訊活動資訊活動資訊活動資訊活動資訊")
+    url = models.CharField(
+        u"說話魚連結",
+        max_length=500,
+        blank=True,
+        default="",
+        help_text=u"可使用站內路徑（如 /zh-hant/open-process）或完整網址"
+    )
+    is_active = models.BooleanField(u"啟用", default=True)
+    updated_at = models.DateTimeField(u"更新時間", auto_now=True)
 
-   
+    def __str__(self):
+        return self.message
 
+    @classmethod
+    def get_solo(cls):
+        obj, _ = cls.objects.get_or_create(
+            pk=1,
+            defaults={
+                "message": "活動資訊活動資訊活動資訊活動資訊活動資訊活動資訊活動資訊活動資訊",
+                "url": "#",
+            },
+        )
+        return obj
+
+    class Meta:
+        verbose_name = u'首頁說話魚設定'
+        verbose_name_plural = u'首頁說話魚設定'
